5ffe2abac56f3b07fc7b32e7f794dc71888f1c06
présibobo <123358803+noaprunaux@users.noreply.github.com>
2026-05-27 10:43:47 +0200

    Delete cours2 directory

--- cours2/atelier_02.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-date = datetime.now().year
-
-prénom = input("Quel est ton prénom ? ")
-age = input("Quel est ton âge ? ")
-annee = date - int(age)
-
-print(f"Bonjour, {prénom}, tu as {age} ans, donc tu es né(e) vers {annee}.")
